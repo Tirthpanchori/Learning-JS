a2df94d0ac2b65969bee425dcd441129ce440592
Tirth panchori <panchoritirth483@gmail.com>
2025-03-11 15:39:36 +0000

    math and num

---
+++ basics/number_and_math.js
@@ -0,0 +1,31 @@
+// n = 100.154
+// console.log(n);
+
+// const num = new Number(100)
+// console.log(num)
+
+// console.log(num.toString())
+// console.log(num.toFixed(2))
+// console.log(n.toPrecision(4))
+
+// const hundred = 1000000
+// console.log(hundred.toLocaleString())
+// console.log(hundred.toLocaleString('en-IN')) // check in mdn docs for more values like 'en-IN'
+
+// +++++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.7))
+// console.log(Math.min(1,2,3,4));
+// console.log(Math.max(1,2,3,4));
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+
+const max = 20
+const min = 10
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
