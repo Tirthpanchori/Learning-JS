6e11f3130d6321961863bb15c5a8d3c213c1fa5f
Tirth panchori <panchoritirth483@gmail.com>
2025-03-11 13:33:40 +0000

    did some changes

--- basics/first.js
+++ basics/first.js
@@ -1,1 +1,8 @@
-console.log("Tirth")
+const accId = 1234
+let accName = "Tirth"
+var accMail = "tirth@mail.com"
+accCity = "Raj"
+let accTag
+// accId = 1
+console.log(accId);
+console.table([accId, accName, accMail, accCity, accTag])
