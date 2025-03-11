b7a52de2a8d3005308cd1d8e691047d922a4e55c
Tirth panchori <panchoritirth483@gmail.com>
2025-03-11 16:14:31 +0000

    got familiar with date and time

---
+++ 01_basics/dates.js
@@ -0,0 +1,24 @@
+var tarik = new Date()
+// console.log(tarik.toString());
+// console.log(tarik.toISOString());
+// console.log(tarik.toJSON());
+// console.log(tarik.toLocaleDateString());
+// console.log(tarik.toLocaleString());
+// console.log(tarik.toDateString());
+
+// const aurtarik = new Date(2024, 1, 24)
+// const aurtariks = new Date(2024, 1, 24, 5, 3)
+// const aurtariks = new Date("2024-04-21")
+const aurtariks = new Date("11-04-2024")
+// console.log(aurtariks.toDateString());
+//console.log(aurtariks.toLocaleString());
+
+const myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(aurtariks.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date() 
+console.log(newDate);
+console.log(newDate.getDate());
+console.log(newDate.getDay());
