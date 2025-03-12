ab16540ff0716cc9936324c1a0059e2e26536d18
Tirth panchori <panchoritirth483@gmail.com>
2025-03-12 03:59:25 +0000

    more fun with array

---
+++ 02_basics/02_array.js
@@ -0,0 +1,28 @@
+const arr1 = ['tirth', 'shiv', 'sarthak','fardeen']
+const arr2 = ['shardul', 'ankit', 'ball', 'adi']
+
+// arr1.push(arr2);
+// console.log(arr1);
+
+// const arr3 = arr1.concat(arr2)
+// console.log(arr3);
+
+// const arr3 = [...arr1, ...arr2]
+// console.log(arr3);
+
+// const arr3 = [1,2,3,[4,5,6], 7,8,[9,1,2,[3,4,5]]]
+// console.log(arr3.flat(Infinity));
+
+
+console.log(Array.isArray("Tirth"));
+console.log(Array.from("Tirth"));
+console.log(Array.from({name: "Tirth"})); // interesting 
+
+
+
+let score1 = 10
+let score2 = 20
+let score3 = 30
+
+console.log(Array.of(score1,score2,score3));
+
